b4ba8c6e36690ea37a7c84eb15ba40e1c3e6dffd
OleFleck <OleFleck@0-208-12>
2020-12-01 15:36:19 +0100

    v2

--- JavaGame2020/src/JavaGame2020.java
+++ JavaGame2020/src/JavaGame2020.java
@@ -3,7 +3,9 @@ public class JavaGame2020 {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World!");
-
+		for (int i = 1; i <= 1000000; i = i + 2) {
+			System.out.println( i + "ist ungerade!");
+		}
 	}
 
 }
